d389b35f99ad100dada4c79be158b46969a1ad6a
James McKay <dcseee-github@yahoo.com.au>
2014-10-25 20:27:26 +1100

    Get working in 1.7.10

--- src/main/java/portablejim/audiodeath/AudioDeath.java
+++ src/main/java/portablejim/audiodeath/AudioDeath.java
@@ -3,26 +3,22 @@ package portablejim.audiodeath;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.GuiGameOver;
-import net.minecraft.client.resources.ResourcePack;
-import net.minecraft.client.resources.ResourcePackRepositoryEntry;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ForgeSubscribe;
-
-import java.util.List;
 
 @Mod(modid = AudioDeath.MODID)
 public class AudioDeath
 {
     public static final String MODID = "audiodeath";
 
-    private boolean audio = true;
+    private int audio = 0;
     
     @SuppressWarnings("UnusedDeclaration")
     @EventHandler
@@ -33,38 +29,20 @@ public class AudioDeath
         }
     }
 
-    @ForgeSubscribe
-    public void onSoundsLoaded(SoundLoadEvent event) {
-        List repositoryEntries = Minecraft.getMinecraft().getResourcePackRepository().getRepositoryEntries();
-        for(Object entry : repositoryEntries.toArray()) {
-            if(entry instanceof ResourcePackRepositoryEntry) {
-                ResourcePackRepositoryEntry repositoryEntry = (ResourcePackRepositoryEntry) entry;
-                ResourcePack r = repositoryEntry.getResourcePack();
-                if(r.resourceExists(new ResourceLocation(MODID, "sound/deathsound.ogg"))) {
-                    event.manager.addSound(MODID + ":deathsound.ogg");
-                }
-                for(int i = 1; i < 10; i++) {
-                    if(r.resourceExists(new ResourceLocation(MODID, "sound/deathsound" + i + ".ogg"))) {
-                        event.manager.addSound(MODID + ":deathsound" + i + ".ogg");
-                    }
-                }
-            }
-        }
-    }
-
     @SuppressWarnings("UnusedDeclaration")
-    @ForgeSubscribe
+    @SubscribeEvent
     public void deathScreen(GuiOpenEvent event) {
+        ResourceLocation deathSoundAudioResource = new ResourceLocation("audiodeath:audiodeath.death");
+        ISound deathSound = PositionedSoundRecord.func_147674_a(deathSoundAudioResource, 1.0F);
         if(event.gui instanceof GuiGameOver) {
-            if(audio) {
-                EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-                //player.playSound(String.format("%s:deathsound", MODID), 1.0F, 1.0F);
-                Minecraft.getMinecraft().sndManager.playSoundFX(String.format("%s:deathsound", MODID), 1.0F, 1.0F);
-                audio = false;
+            if(audio == 0) {
+                Minecraft.getMinecraft().getSoundHandler().playSound(deathSound);
+                audio = 1;
             }
         }
         else {
-            audio = true;
+            Minecraft.getMinecraft().getSoundHandler().stopSound(deathSound);
+            audio = 0;
         }
     }
 }
